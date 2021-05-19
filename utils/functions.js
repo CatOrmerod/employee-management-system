5bad033ed62d72f6ef857e17c5f241e9ed6b8d2f
Cat Ormerod <catormerod@gmail.com>
2021-05-19 11:09:25 +1000

    First function added

--- utils/functions.js
+++ utils/functions.js
@@ -1,2 +1,14 @@
 const consoletable = require('console.table')
 const inquirer = require('inquirer')
+const mysql = require('mysql');
+const { promptUser } = require('.');
+
+const viewDepartments = () => {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, res) => {
+        console.table(rows)
+        promptUser();
+    });
+}
+
+module.export = { viewDepartments }
--- utils/index.js
+++ utils/index.js
@@ -1,78 +1,78 @@
 const inquirer = require('inquirer')
+const functions = require('/functions')
 
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'initial list',
-            message: 'What would you like to do?',
-            choices: [
-                'View all Employees',
-                'View all Employees by Department',
-                'View all Employees by Manager',
-                'Add Employee',
-                'Remove Employee',
-                'Update Employee Role',
-                'Update Employee Manager',
-                'View all Roles',
-                'Add Role',
-                'Remove Role',
-                'View all Departments',
-                'Add Department',
-                'Remove Department',
-                'View Department budget',
-                'Exit',
-            ]
-        }
-    ])
-        .then(function (answers) {
-            const choices = answers;
-            if (choices === 'View all Employees') {
-                viewEmployees();
-            }
-            if (choices === 'View all Employees by Department') {
-                viewEmployeesDept();
-            }
-            if (choices === 'View all Employees by Manager') {
-                viewEmployeesMan();
-            }
-            if (choices === 'Add Employee') {
-                addEmployee();
-            }
-            if (choices === 'Remove Employee') {
-                removeEmployee();
-            }
-            if (choices === 'Update Employee Role') {
-                updateEmployeeRole();
-            }
-            if (choices === 'Update Employee Manager') {
-                updateEmployeeMan();
-            }
-            if (choices === 'View all Roles') {
-                viewRoles();
-            }
-            if (choices === 'Add Role') {
-                addRole();
-            }
-            if (choices === 'Remove Role') {
-                removeRole();
-            }
-            if (choices === 'View all Departments') {
-                viewDepartments();
-            }
-            if (choices === 'Add Department') {
-                addDepartment();
-            }
-            if (choices === 'Remove Department') {
-                removeDepartment();
-            }
-            if (choices === 'View Department budget') {
-                viewDepartmentBudget();
-            }
-            if (choices === 'Exit') {
-                endPrompts();
-            }
-        })
-}
+    return inquirer.prompt({
+        type: 'list',
+        name: 'initial list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all Employees',
+            'View all Employees by Department',
+            'View all Employees by Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+            'View all Roles',
+            'Add Role',
+            'Remove Role',
+            'View all Departments',
+            'Add Department',
+            'Remove Department',
+            'View Department budget',
+            'Exit',
+        ],
+    })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all Employees':
+                    viewEmployees();
+                    break;
+                case 'View all Employees by Department':
+                    viewEmployeesDept();
+                    break;
+                case 'View all Employees by Manager':
+                    viewEmployeesMan();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Remove Employee':
+                    removeEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'Update Employee Manager':
+                    updateEmployeeMan();
+                    break;
+                case 'View all Roles':
+                    viewRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Remove Role':
+                    removeRole();
+                    break;
+                case 'View all Departments':
+                    functions.viewDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Remove Department':
+                    removeDepartment();
+                    break;
+                case 'View Department budget':
+                    viewDepartmentBudget();
+                    break;
+                case 'Exit':
+                    endPrompts();
+                    break;
 
-    module.exports = { promptUser }
+            }
+        });
+};
+module.exports = { promptUser }
