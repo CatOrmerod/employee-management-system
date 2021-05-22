3a1faffa1be84f67173dc23d646551e681dc260a
Cat Ormerod <catormerod@gmail.com>
2021-05-22 10:01:35 +1000

    latest version of confirmPrompt

--- utils/confirmPrompt.js
+++ utils/confirmPrompt.js
@@ -2,8 +2,7 @@ const inquirer = require("inquirer")
 const functions = require('./functions')
 //const { promptUser } = require('./prompts')
 
-
-const confirmPrompt = () => {
+module.exports.confirmPrompt = () => {
     return inquirer.prompt({
         type: 'confirm',
         name: 'confirm',
@@ -17,8 +16,22 @@ const confirmPrompt = () => {
     }) 
 }
 
-const promptHandler = () => {
-    console.log("prompt handler called", promptUser)
+// const confirmPrompt = () => {
+//     return inquirer.prompt({
+//         type: 'confirm',
+//         name: 'confirm',
+//         message: 'Another Request or Exit?'
+//     })
+//     .then((answer) => {
+//         console.log(answer.confirm);
+//         if (answer.confirm) 
+//         promptHandler();
+//         else console.log('Programme finished')
+//     }) 
+// }
+
+module.exports.promptHandler = () => {
+    console.log("prompt handler called")
     return inquirer.prompt({
         type: 'list',
         name: 'action',
@@ -91,4 +104,4 @@ const promptHandler = () => {
         });
 };
 
-module.exports = { confirmPrompt }
+//module.exports = { confirmPrompt, promptHandler }
